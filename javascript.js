c9ad32254810a39dca67600492276a4ba226cee6
Harsha <harsha.lv2006@gmail.com>
2025-06-18 12:54:36 +0530

    added logic

---
+++ javascript.js
@@ -0,0 +1,47 @@
+let humanScore=0;
+let computerScore=0;
+
+function getComputerChoice(){
+    let choice=Math.floor(Math.random()*3);
+    switch(choice){
+        case 0: return "rock"; break;
+        case 1: return "paper"; break;
+        case 2: return "scissor"; break;
+    }
+}
+
+function getHumanChoice(){
+    let choice=prompt("rock, paper or scissors?").toLowerCase();
+    if ((choice!=="rock" && choice!=="paper" && choice!=="scissor") || choice===null){
+        return "rock";
+    }
+    return choice;
+}
+
+function playGame(){
+    for (let i=0;i<5;i++){
+        let computerChoice=getComputerChoice();
+        let humanChoice=getHumanChoice();
+        if (computerChoice===humanChoice){
+            console.log(`Computer chose: ${computerChoice}`);
+            console.log("Draw");
+        }
+        else if ((computerChoice==="rock" && humanChoice==="scissor") || (computerChoice==="paper" && humanChoice==="rock") ||
+        (computerChoice==="scissor" && humanChoice==="paper")){
+            console.log(`Computer chose: ${computerChoice}`);
+            console.log("You lost");
+            computerScore++;
+        }
+        else if ((humanChoice==="rock" && computerChoice==="scissor") || (humanChoice==="paper" && computerChoice==="rock") ||
+        (humanChoice==="scissor" && computerChoice==="paper")){
+            console.log(`Computer chose: ${computerChoice}`);
+            console.log("You won!");
+            humanScore++;
+        }
+    }
+
+    console.log(`Final score: ${humanScore}/5`);
+}
+
+playGame();
+
