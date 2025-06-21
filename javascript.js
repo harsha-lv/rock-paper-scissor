fa426d38dfb01db5492d6f5f58fb3a8ec52a8b70
Harsha <harsha.lv2006@gmail.com>
2025-06-21 14:13:35 +0530

    refactor logic

--- javascript.js
+++ javascript.js
@@ -1,5 +1,16 @@
 let humanScore=0;
-let computerScore=0;
+let rounds=0;
+
+let outputElement=document.querySelector(".output");
+let headerElement=document.querySelector(".header");
+let choicesElement=document.createElement("div");
+choicesElement.classList.add("choices");
+let resElement=document.createElement("div");
+resElement.classList.add("result");
+let scoreElement=document.createElement("div");
+scoreElement.classList.add("score");
+let btns=document.querySelectorAll("button");
+let bodyElement=document.querySelector("body");
 
 function getComputerChoice(){
     let choice=Math.floor(Math.random()*3);
@@ -10,45 +21,70 @@ function getComputerChoice(){
     }
 }
 
-function getHumanChoice(){
-    let choice;
-    let btns=document.querySelectorAll("button");
-    btns.forEach(button =>{
-        button.addEventListener("click", (e) => {
-            switch(e.target.id){
-                case "rock": choice="rock"; break;
-                case "paper": choice="paper"; break;
-                case "scissor": choice="scissor"; break;
-            }
-        })
-    })
-    return choice;
-}
-
-function playGame(){
-    for (let i=0;i<5;i++){
-        let computerChoice=getComputerChoice();
-        let humanChoice=getHumanChoice();
-        if (computerChoice===humanChoice){
-            console.log(`Computer chose: ${computerChoice}`);
-            console.log("Draw");
-        }
-        else if ((computerChoice==="rock" && humanChoice==="scissor") || (computerChoice==="paper" && humanChoice==="rock") ||
-        (computerChoice==="scissor" && humanChoice==="paper")){
-            console.log(`Computer chose: ${computerChoice}`);
-            console.log("You lost");
-            computerScore++;
-        }
-        else if ((humanChoice==="rock" && computerChoice==="scissor") || (humanChoice==="paper" && computerChoice==="rock") ||
-        (humanChoice==="scissor" && computerChoice==="paper")){
-            console.log(`Computer chose: ${computerChoice}`);
-            console.log("You won!");
-            humanScore++;
-        }
+function playGame(humanChoice, computerChoice){
+    let result;
+    choicesElement.textContent = `You chose:${humanChoice}\nComputer chose:${computerChoice}`;
+    outputElement.appendChild(choicesElement);
+    if (computerChoice===humanChoice){
+        result="draw";
     }
-
-    console.log(`Final score: ${humanScore}/5`);
+    else if ((computerChoice==="rock" && humanChoice==="scissor") || (computerChoice==="paper" && humanChoice==="rock") ||
+    (computerChoice==="scissor" && humanChoice==="paper")){
+        result="computer";
+    }
+    else if ((humanChoice==="rock" && computerChoice==="scissor") || (humanChoice==="paper" && computerChoice==="rock") ||
+    (humanChoice==="scissor" && computerChoice==="paper")){
+        result="human";
+    }
+    output(result)
 }
 
-playGame();
+function output(result){
+    switch(result){
+        case "draw":
+            resElement.textContent=`Draw`;
+            break;
+        case "computer":
+            resElement.textContent=`Computer wins`;
+            break;
+        case "human":
+            resElement.textContent=`You win!`;
+            humanScore++;
+            break;
+    }
+    scoreElement.textContent=`Score: ${humanScore}`;
+    headerElement.appendChild(scoreElement);
+    outputElement.appendChild(resElement);
+}
+
+function endGame(){
+    headerElement.remove();
+    btns.forEach(btn=>btn.remove());
+    outputElement.remove();
+
+    let endGameBody=document.createElement("div");
+    endGameBody.classList.add("endGameBody");
+    bodyElement.appendChild(endGameBody);
+
+    let endGameElement=document.createElement("div");
+    endGameElement.textContent="Game Over";
+
+    let endGameScore=document.createElement("div");
+    endGameScore.textContent=`Score: ${humanScore}`;
+    
+    endGameBody.appendChild(endGameElement);
+    endGameBody.appendChild(endGameScore);
+}
+
+btns.forEach(button =>{
+    button.addEventListener("click", (e) =>{
+        let computerChoice=getComputerChoice();
+        const humanChoice=e.target.id;
+        playGame(humanChoice, computerChoice);
+        if (humanScore>=5){
+            endGame();
+        }
+    })
+})
+
 
